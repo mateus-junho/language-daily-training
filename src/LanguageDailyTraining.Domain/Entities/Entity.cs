3257311e18aaa3e5b0416d5a5bae733ef67a04eb
Mateus Junho <mateus.junho@farfetch.com>
2021-10-14 15:29:28 -0300

    Add project files.

---
+++ src/LanguageDailyTraining.Domain/Entities/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LanguageDailyTraining.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+    }
+}
